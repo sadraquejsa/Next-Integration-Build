002c3cf960446f4552af6af046ed553a9c6cedfd
Sadrauque José <sadraque.jose@gmail.com>
2018-06-07 20:14:40 -0300

    Alteração no diretorio do projeto

---
+++ src/main/java/br/com/next/integration/nextintegration/NextIntegrationApplication.java
@@ -0,0 +1,12 @@
+package br.com.next.integration.nextintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/next/integration/nextintegration/NextIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.next.integration.nextintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NextIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
